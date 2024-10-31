2c9b3d4d551f96807909185ef1dc22085c0b8ea5
msayrac <msayracode@gmail.com>
2024-10-31 09:31:00 +0300

    Api_Dapper connection is success

---
+++ RealEstate_Dapper_Api/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+		public int CategoryID { get; set; }
+		public string CategoyName { get; set; }
+		public bool CategoryStatus { get; set; }
+
+	}
+}
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private readonly Context _context;
+
+		public CategoryRepository(Context context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+		{
+			string query = "Select * From Category";
+
+			using (var connection = _context.CreateConnection())
+			{
+				var values = await connection.QueryAsync<ResultCategoryDto>(query);
+
+				return values.ToList();
+			}
+
+
+
+
+		}
+	}
+}
