a5c914ad531a8046c4d0556e4f3ace05c6df380c
msayrac <msayracode@gmail.com>
2024-11-12 20:15:24 +0300

    WhoWeAreDetail Api added

---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/IWhoWeAreRepository.cs
@@ -0,0 +1,18 @@
+﻿using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
+{
+    public interface IWhoWeAreRepository
+    {
+        Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync();
+        void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto);
+
+        void DeleteWhoWeAreDetail(int id);
+
+        void UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto);
+
+        Task<GetByIDWhoWeAreDetailDto> GetGetByIDWhoWeAreDetail(int id);
+
+    }
+}
---
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/WhoWeAreRepository.cs
@@ -0,0 +1,34 @@
+﻿using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
+{
+    public class WhoWeAreRepository : IWhoWeAreRepository
+    {
+
+        // Video 14 Dakika 11:40 da kaldım
+        public void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteWhoWeAreDetail(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIDWhoWeAreDetailDto> GetGetByIDWhoWeAreDetail(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateWhoWeAreDetail(UpdateWhoWeAreDetailDto updateWhoWeAreDetailDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
