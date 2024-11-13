093c21129aebf483f37ecbde4a087fe85893f084
msayrac <msayracode@gmail.com>
2024-11-13 12:36:19 +0300

    WhoWeAreDetail create and delete repository added

--- RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/WhoWeAreRepository.cs
+++ RealEstate_Dapper_Api/Repositories/WhoWeAreRepository/WhoWeAreRepository.cs
@@ -1,19 +1,48 @@
-﻿using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using RealEstate_Dapper_Api.Dtos.WhoWeAreDetailDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
 
 namespace RealEstate_Dapper_Api.Repositories.WhoWeAreRepository
 {
     public class WhoWeAreRepository : IWhoWeAreRepository
     {
+        private readonly Context _context;
 
-        // Video 14 Dakika 11:40 da kaldım
-        public void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
+        public WhoWeAreRepository(Context context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public void DeleteWhoWeAreDetail(int id)
+        public async void CreateWhoWeAreDetail(CreateWhoWeAreDetailDto createWhoWeAreDetailDto)
         {
-            throw new NotImplementedException();
+
+            string query = "insert into WhoWeAreDetail (Title,SubTitle,Description1,Description1) values (@title,@subTitle,@description1,@description2)";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@title", createWhoWeAreDetailDto.Title);
+            parameters.Add("@subTitle", createWhoWeAreDetailDto.Subtitle);
+            parameters.Add("@description1", createWhoWeAreDetailDto.Description1);
+            parameters.Add("@description2", createWhoWeAreDetailDto.Description2);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+
+        }
+
+        public async void DeleteWhoWeAreDetail(int id)
+        {
+            string query = "Delete From WhoWeAreDetail Where WhoWeAreDetailID=@whoWeAreDetailID";
+
+            var parameters = new DynamicParameters();
+
+            parameters.Add("@whoWeAreDetailID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
         }
 
         public Task<List<ResultWhoWeAreDetailDto>> GetAllWhoWeAreDetailAsync()
