f6ea845a1c754446e6d43822e330720d3796d3ed
msayrac <msayracode@gmail.com>
2024-11-19 23:27:58 +0300

    Service Api and Controllers are created

--- RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultServicesComponentPartial.cs
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultServicesComponentPartial.cs
@@ -4,8 +4,21 @@ namespace RealEstate_Dapper_UI.ViewComponents.HomePage
 {
     public class _DefaultServicesComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private IHttpClientFactory _httpClientFactory;
+
+        public _DefaultServicesComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage =await client.GetAsync("");
+
+
+
             return View();
         }
 
