ce09d67f56efd8e692ff67986da344aa4402ccfd
msayrac <msayracode@gmail.com>
2024-11-23 16:10:38 +0300

    Testimonial Api added and consumed in UI

--- RealEstate_Dapper_Api/Repositories/ServiceRepository/IServiceRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ServiceRepository/IServiceRepository.cs
@@ -8,23 +8,9 @@ namespace RealEstate_Dapper_Api.Repositories.ServiceRepository
 
         Task<List<ResultServiceDto>> GetAllServiceAsync();
         void CreateService(CreateServiceDto createServiceDto);
-
         void DeleteService(int id);
-
         void UpdateService(UpdateServiceDto updateServiceDto);
-
         Task<GetByIDServiceDto> GetService(int id);
-
-
-
-
-
-
-
-
-
-
-
 
     }
 }
---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultOurTestimonialComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.TestimonialDtos;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+	public class _DefaultOurTestimonialComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public _DefaultOurTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responsemessage = await client.GetAsync("https://localhost:44373/api/Testimonials");
+
+			if (responsemessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responsemessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+
+				return View(values);
+			}
+			return View();
+		}
+	}
+}
