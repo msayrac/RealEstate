1f903a709a36c8bc52a2163ee7953edcb4c8ca68
msayrac <msayracode@gmail.com>
2024-11-29 18:09:54 +0300

    Product Api is consumed

---
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,18 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.ProductDtos
+{
+	public class CreateProductDto
+	{
+		public string title { get; set; }
+		public decimal price { get; set; }
+		public string city { get; set; }
+		public string district { get; set; }
+		public string categoryName { get; set; }
+		public string CoverImage { get; set; }
+		public string Type { get; set; }
+		public string Address { get; set; }
+
+
+
+
+	}
+}
---
+++ RealEstate_Dapper_UI/Dtos/ProductDtos/ResultProductDto.cs
@@ -0,0 +1,17 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.ProductDtos
+{
+	public class ResultProductDto
+	{
+		public int productID { get; set; }
+		public string title { get; set; }
+		public decimal price { get; set; }
+		public string city { get; set; }
+		public string district { get; set; }
+		public string categoryName { get; set; }
+		public string CoverImage { get; set; }
+		public string Type { get; set; }
+		public string Address { get; set; }
+
+
+	}
+}
