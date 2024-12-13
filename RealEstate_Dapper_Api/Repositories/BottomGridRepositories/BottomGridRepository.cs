ccd7fb839e07426deac1f61dd214356e440f17b4
msayrac <msayracode@gmail.com>
2024-12-13 10:10:05 +0300

    BottomGrid Api controller is created and consumed

--- RealEstate_Dapper_Api/Repositories/BottomGridRepositories/BottomGridRepository.cs
+++ RealEstate_Dapper_Api/Repositories/BottomGridRepositories/BottomGridRepository.cs
@@ -4,46 +4,84 @@ using RealEstate_Dapper_Api.Models.DapperContext;
 
 namespace RealEstate_Dapper_Api.Repositories.BottomGridRepositories
 {
-    public class BottomGridRepository : IBottomGridRepository
-    {
-        private readonly Context _context;
+	public class BottomGridRepository : IBottomGridRepository
+	{
+		private readonly Context _context;
 
-        public BottomGridRepository(Context context)
-        {
-            _context = context;
-        }
+		public BottomGridRepository(Context context)
+		{
+			_context = context;
+		}
 
-        public void CreateBottomGrid(CreateBottomGridDto createBottomGridDto)
-        {
+		public async void CreateBottomGrid(CreateBottomGridDto createBottomGridDto)
+		{
+			string query = "insert into BottomGrid (Icon,Title,Description) values (@icon, @title,@description)";
 
-            throw new NotImplementedException();
-        }
+			var parameters = new DynamicParameters();
+			parameters.Add("@icon", createBottomGridDto.Icon);
+			parameters.Add("@title", createBottomGridDto.Title);
+			parameters.Add("@description", createBottomGridDto.Description);
 
-        public void DeleteBottomGrid(int id)
-        {
-            throw new NotImplementedException();
-        }
+			using (var connection = _context.CreateConnection())
+			{
+				await connection.ExecuteAsync(query, parameters);
+			}
+		}
 
-        public async Task<List<ResultBottomGridDto>> GetAllBottomGridAsync()
-        {
-            string query = "Select * From BottomGrid";
+		public async void DeleteBottomGrid(int id)
+		{
+			string query = "Delete From BottomGrid Where BottomGridID=@bottomGridID";
 
-            using (var connection = _context.CreateConnection())
-            {
-                var values = await connection.QueryAsync<ResultBottomGridDto>(query);
-                return values.ToList();
-            }
+			var parameter = new DynamicParameters();
+			parameter.Add("@bottomGridID", id);
 
-        }
+			using (var connection = _context.CreateConnection())
+			{
+				await connection.ExecuteAsync(query, parameter);
+			}
+		}
 
-        public Task<GetBottomGridDto> GetBottomGrid(int id)
-        {
-            throw new NotImplementedException();
-        }
+		public async Task<List<ResultBottomGridDto>> GetAllBottomGridAsync()
+		{
+			string query = "Select * From BottomGrid";
 
-        public void UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto)
-        {
-            throw new NotImplementedException();
-        }
-    }
+			using (var connection = _context.CreateConnection())
+			{
+				var values = await connection.QueryAsync<ResultBottomGridDto>(query);
+				return values.ToList();
+			}
+
+		}
+
+		public async Task<GetBottomGridDto> GetBottomGrid(int id)
+		{
+			string query = "Select * from BottomGrid Where BottomGridID=@bottomGridID";
+
+			var parameter = new DynamicParameters();
+			parameter.Add("@bottomGridID", id);
+
+			using (var connection = _context.CreateConnection())
+			{
+				var values = await connection.QueryFirstOrDefaultAsync<GetBottomGridDto>(query, parameter);
+				return values;
+			}
+		}
+
+		public async void UpdateBottomGrid(UpdateBottomGridDto updateBottomGridDto)
+		{
+			string query = "Update BottomGrid Set Icon=@icon,Title=@title,Description=@description Where BottomGridID=@bottomGridID";
+
+			var parameters = new DynamicParameters();
+			parameters.Add("@icon", updateBottomGridDto.Icon);
+			parameters.Add("@title", updateBottomGridDto.Title);
+			parameters.Add("@description", updateBottomGridDto.Description);
+			parameters.Add("@bottomGridID", updateBottomGridDto.BottomGridID);
+
+			using (var connection = _context.CreateConnection())
+			{
+				await connection.ExecuteAsync(query, parameters);
+			}
+
+		}
+	}
 }
