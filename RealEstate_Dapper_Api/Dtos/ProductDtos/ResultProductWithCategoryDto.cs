f44ad54fa2a79a47cdef01a628a3fff39977fcaf
msayrac <msayracode@gmail.com>
2024-12-15 16:17:52 +0300

    DealOfTheDay Api added

--- RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
+++ RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductWithCategoryDto.cs
@@ -11,9 +11,10 @@
         public string CoverImage { get; set; }
         public string Type { get; set; }
         public string Address { get; set; }
+		public bool DealOfTheDay { get; set; }
 
 
 
 
-    }
+	}
 }
