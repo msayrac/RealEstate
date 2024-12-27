d81a55007e5e5226cc68935d53cc4bb7ff06239d
msayrac <msayracode@gmail.com>
2024-12-27 18:50:44 +0300

    Jwt Token added

---
+++ RealEstate_Dapper_Api/Tools/GetCheckedAppUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+	public class GetCheckedAppUserViewModel
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Role { get; set; }
+		public string IsExist { get; set; }
+	}
+}
---
+++ RealEstate_Dapper_Api/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,17 @@
+﻿namespace RealEstate_Dapper_Api.Tools
+{
+	public class JwtTokenDefaults
+	{
+		public const string ValidAudience = "https://localhost";
+		public const string ValidIssuer = "https://localhost";
+		public const string Key = "Realestate..0102030405Asp.NetCore8.0.1+-*/";
+		public const int Expire = 5;
+
+
+
+
+
+
+
+	}
+}
