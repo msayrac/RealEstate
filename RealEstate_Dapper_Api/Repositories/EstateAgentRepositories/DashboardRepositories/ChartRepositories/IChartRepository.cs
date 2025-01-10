787ccdd943b08ab17cbb5598ca82415b209a3f9d
msayrac <msayracode@gmail.com>
2025-01-10 09:41:30 +0300

    Dashboard Cahrt added for RealEstateAgent

---
+++ RealEstate_Dapper_Api/Repositories/EstateAgentRepositories/DashboardRepositories/ChartRepositories/IChartRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealEstate_Dapper_Api.Dtos.ChartDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories
+{
+	public interface IChartRepository
+	{
+		Task<List<ResultChartDto>> Get5CityForChart();
+
+
+	}
+}
