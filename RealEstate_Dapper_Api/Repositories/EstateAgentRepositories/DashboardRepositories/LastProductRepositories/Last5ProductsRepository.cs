79d5771e1cf562109c6ab5cff1008f5fb8350b54
msayrac <msayracode@gmail.com>
2025-01-12 11:46:45 +0300

    Last5ProductsRepository added for RealEstateEmployee

---
+++ RealEstate_Dapper_Api/Repositories/EstateAgentRepositories/DashboardRepositories/LastProductRepositories/Last5ProductsRepository.cs
@@ -0,0 +1,13 @@
+﻿using RealEstate_Dapper_Api.Dtos.ProductDtos;
+
+namespace RealEstate_Dapper_Api.Repositories.EstateAgentRepositories.DashboardRepositories.LastProductRepositories
+{
+    public class Last5ProductsRepository : ILast5ProductsRepository
+    {
+        public Task<List<ResultLast5ProductWithCategoryDto>> GetLast5ProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
