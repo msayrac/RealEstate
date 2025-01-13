a2026c1d74ff7c89c956675a8929a75b8cbfe2ad
msayrac <msayracode@gmail.com>
2025-01-13 20:47:47 +0300

    LayoutEstateAgent Index updated

---
+++ RealEstate_Dapper_UI/ViewComponents/EstateAgent/_EstateAgentNavbarComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.EstateAgent
+{
+    public class _EstateAgentNavbarComponentPartial : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+
+
+    }
+}
