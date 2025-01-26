6984f7fc00373931095a388c65ed28e9abe48ebb
msayrac <msayracode@gmail.com>
2025-01-26 14:34:33 +0300

    ProductImageRepository added

---
+++ RealEstate_Dapper_Api/Repositories/ProductImageRepositories/ProductImageRepository.cs
@@ -0,0 +1,20 @@
+﻿using RealEstate_Dapper_Api.Dtos.ProductImageDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.ProductImageRepositories
+{
+    public class ProductImageRepository : IProductImageRepository
+    {
+        private readonly Context _context;
+
+        public ProductImageRepository(Context context)
+        {
+            _context = context;
+        }
+
+        public Task<GetProductImageByProductIdDto> GetProductImageByProductId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
