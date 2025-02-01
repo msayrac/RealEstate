a62f742dea356e945558de5af7788b443c69d4ac
msayrac <msayracode@gmail.com>
2025-02-01 14:38:13 +0300

    Amenity Consume and Map Video Embed

--- RealEstate_Dapper_UI/Controllers/PropertyController.cs
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -5,82 +5,84 @@ using RealEstate_Dapper_UI.Dtos.ProductDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
-    public class PropertyController : Controller
-    {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public PropertyController(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
+	public class PropertyController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public PropertyController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44373/api/Products/ProductListWithCategory");
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44373/api/Products/ProductListWithCategory");
 
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
-                return View(values);
-            }
-            return View();
-        }
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
 
 
-        [HttpGet]
-        public async Task<IActionResult> PropertySingle(int id)
-        {
-            id = 1;
+		[HttpGet]
+		public async Task<IActionResult> PropertySingle(int id)
+		{
+			id = 1;
 
-            var client = _httpClientFactory.CreateClient();
+			var client = _httpClientFactory.CreateClient();
 
-            var responseMessage = await client.GetAsync("https://localhost:44373/api/Products/GetProductByProductId?id=" + id);
-            var jsonData = await responseMessage.Content.ReadAsStringAsync();
-            var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
+			var responseMessage = await client.GetAsync("https://localhost:44373/api/Products/GetProductByProductId?id=" + id);
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<ResultProductDto>(jsonData);
 
-            var client2 = _httpClientFactory.CreateClient();
-            var responseMessage2 = await client2.GetAsync("https://localhost:44373/api/ProductDetails/GetProductDetailByProductId?id=" + id);
-            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
-            var values2 = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonData2);
+			var client2 = _httpClientFactory.CreateClient();
+			var responseMessage2 = await client2.GetAsync("https://localhost:44373/api/ProductDetails/GetProductDetailByProductId?id=" + id);
+			var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+			var values2 = JsonConvert.DeserializeObject<GetProductDetailByIdDto>(jsonData2);
 
-            ViewBag.productId = values.productID;
-            ViewBag.title1 = values.title.ToString();
-            ViewBag.price = values.price;
-            ViewBag.city = values.city;
-            ViewBag.district = values.district;
-            ViewBag.Address = values.Address;
-            ViewBag.type = values.Type;
-            ViewBag.description = values.description;
+			ViewBag.productId = values.productID;
+			ViewBag.title1 = values.title.ToString();
+			ViewBag.price = values.price;
+			ViewBag.city = values.city;
+			ViewBag.district = values.district;
+			ViewBag.Address = values.Address;
+			ViewBag.type = values.Type;
+			ViewBag.description = values.description;
 			ViewBag.date = values.AdvertisementDate;
 
 
 			ViewBag.bathCount = values2.bathCount;
-            ViewBag.bedCount = values2.bedRoomCount;
-            ViewBag.size = values2.productSize;
-            ViewBag.roomCount = values2.roomCount;
-            ViewBag.garageCount = values2.garageSize;
-            ViewBag.builtYear = values2.buildYear;
-            
-
-
-            DateTime date1 = DateTime.Now;
-            DateTime date2 = values.AdvertisementDate;
-
-            TimeSpan timeSpan = date1 - date2;
-            int month = timeSpan.Days;
-            ViewBag.datediff = month / 30;
-
-            return View();
-
-        }
+			ViewBag.bedCount = values2.bedRoomCount;
+			ViewBag.size = values2.productSize;
+			ViewBag.roomCount = values2.roomCount;
+			ViewBag.garageCount = values2.garageSize;
+			ViewBag.builtYear = values2.buildYear;
+			ViewBag.location = values2.location;
+			ViewBag.videoUrl = values2.videoUrl;
 
 
 
+			DateTime date1 = DateTime.Now;
+			DateTime date2 = values.AdvertisementDate;
+
+			TimeSpan timeSpan = date1 - date2;
+			int month = timeSpan.Days;
+			ViewBag.datediff = month / 30;
+
+			return View();
+
+		}
 
 
 
 
 
-    }
+
+
+
+	}
 }
