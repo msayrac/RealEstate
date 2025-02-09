ea78132034f65303bb6a740d11b486d5d5af60a1
msayrac <msayracode@gmail.com>
2025-02-09 16:42:19 +0300

    PopularLocaitonDto edited

--- RealEstate_Dapper_Api/Dtos/PopularLocationDtos/ResultPopularLocationDto.cs
+++ RealEstate_Dapper_Api/Dtos/PopularLocationDtos/ResultPopularLocationDto.cs
@@ -2,8 +2,9 @@
 {
 	public class ResultPopularLocationDto
 	{
-        public int LocationID { get; set; }
-        public string CityName { get; set; }
-        public string ImageUrl { get; set; }               
+		public int LocationID { get; set; }
+		public string CityName { get; set; }
+		public string ImageUrl { get; set; }
+		public int PropertyCount { get; set; }
 	}
 }
